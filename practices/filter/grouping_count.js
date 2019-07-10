81dca1e5d32358537daede86470efff42ade8e95
SEAN H F HE (ITA-ISDC-ISD-OOCLL/ZHA) <HESE2@OOCL.COM>
2019-07-10 15:48:15 +0800

    finish grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,12 @@
 function grouping_count(collection) {
 
   //implement here
+  const result={};
+  collection.filter((number) => {
+    result[number]=result[number]>=1?result[number]+1:1;
+    return true;
+  });
+  return result;
 }
 
 module.exports = grouping_count;
