1ef12dcef405af5422c4484282e1b851f9f24a3f
SEAN H F HE (ITA-ISDC-ISD-OOCLL/ZHA) <HESE2@OOCL.COM>
2019-07-10 14:50:33 +0800

    finish practices filter choose_even and choose_multiples_of_three

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,8 @@
 function choose_even(collection) {
 
   //implement here
+  const selectEvenNumber=(number)=>number%2===0;
+  return collection.filter(selectEvenNumber);
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,8 @@
 function choose_multiples_of_three(collection) {
 
   //implement here
+  const selectMultiplesThree=(number)=>number%3===0;
+  return collection.filter(selectMultiplesThree);
 }
 
 module.exports = choose_multiples_of_three;
